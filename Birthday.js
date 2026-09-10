9fdc14463c9adff330dbb5c39cc1d7888d31961c
missonpixel7-lgtm <missonpixel7@gmail.com>
2026-09-10 08:58:16 +0300

    Update Birthday.js

--- Birthday.js
+++ Birthday.js
@@ -10,7 +10,7 @@ var w = (c.width = window.innerWidth),
   hw = w / 2, // half-width
   hh = h / 2,
   opts = {
-    strings: ["HAPPY", "BIRTHDAY!", "My Life"],
+    strings: ["HAPPY", "BIRTHDAY!", "My In Complete Love Rothi"],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
